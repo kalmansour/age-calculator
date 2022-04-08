746d42b2368867d49949fe811005f6e43a8388dc
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-04-08 14:12:14 +0300

    get date of birth logic, get age logic and main function logic and age validator

--- age_calculator.py
+++ age_calculator.py
@@ -1,18 +1,35 @@
 from datetime import date
+import datetime
+from datetime import timedelta
 
 
 def get_dob():
-    # write code here
+	birth_year = int(input("Enter birth year:"))
+	birth_month = int(input("Enter birth month:"))
+	birth_day = int(input("Enter birth day:"))
+	dob = datetime.date(birth_year, birth_month, birth_day)
+	return dob
 	...
 
 
 def get_age(dob):
-    # write code here
+	dob_year = dob.year
+	today = date.today().year
+	age = today - dob_year
+	return age
 	...
 
 
 def main():
-	# write code here
+	dob = get_dob()
+	today = date.today()
+	
+	if dob > today:
+		print("Date of birth is invalid")
+	else:
+		age = get_age(dob)
+		print("Age =", age)
+	
 	...
 
 
